96a01017df977671d5b03cf2fb6286b325c446de
Benjamin Jeanjean <bjeanjean@thegobeliners.com>
2017-12-08 14:04:51 +0100

    Define method to get Time Totals

--- src/Time.php
+++ src/Time.php
@@ -42,7 +42,7 @@ class Time extends Model
         $id = 0;
         if (!empty($data['task_id'])) {
             $id = (int) $data['task_id'];
-            $resource = 'todo_items';
+            $resource = 'tasks';
         } elseif (!empty($data['project_id'])) {
             $id = (int) $data['project_id'];
             $resource = 'projects';
@@ -65,9 +65,8 @@ class Time extends Model
      *
      * @param string $id
      * @param array $params
-     * @return TeamWorkPm\Response\Model
+     * @return \TeamWorkPm\Response\Model
      */
-
     public function getAll(array $params = [])
     {
         return $this->rest->get("$this->action", $params);
@@ -83,9 +82,28 @@ class Time extends Model
      * TODATE : string (YYYYMMDD) - The end date to retrieve to
      * TOTIME : string (HH:MM) - The end time only if TODATE is passed
      *
+     * @param string $id
+     * @param array $params
+     * @return \TeamWorkPm\Response\Model
+     */
+    public function getTotalAll(array $params = [])
+    {
+        return $this->rest->get("time/total", $params);
+    }
+
+    /**
+     * Optional Parameters
+     *
+     * PAGE : numeric - The page to start retrieving entries from
+     * ( e.g: page=1 gives records 1 - 50, page=2 gives records 51-99 etc)
+     * FROMDATE : string (YYYYMMDD) - The start date to retrieve from
+     * FROMTIME : string (HH:MM) - The start time only if FROMDATE is passed
+     * TODATE : string (YYYYMMDD) - The end date to retrieve to
+     * TOTIME : string (HH:MM) - The end time only if TODATE is passed
+     *
      * @param int $id
      * @param array $params
-     * @return TeamWorkPm\Response\Model
+     * @return \TeamWorkPm\Response\Model
      */
     public function getByProject($project_id, array $params = [])
     {
@@ -97,6 +115,29 @@ class Time extends Model
     }
 
     /**
+     * Optional Parameters
+     *
+     * PAGE : numeric - The page to start retrieving entries from
+     * ( e.g: page=1 gives records 1 - 50, page=2 gives records 51-99 etc)
+     * FROMDATE : string (YYYYMMDD) - The start date to retrieve from
+     * FROMTIME : string (HH:MM) - The start time only if FROMDATE is passed
+     * TODATE : string (YYYYMMDD) - The end date to retrieve to
+     * TOTIME : string (HH:MM) - The end time only if TODATE is passed
+     *
+     * @param int $id
+     * @param array $params
+     * @return \TeamWorkPm\Response\Model
+     */
+    public function getTotalByProject($project_id, array $params = [])
+    {
+        $project_id = (int) $project_id;
+        if ($project_id <= 0) {
+            throw new Exception('Invalid param project_id');
+        }
+        return $this->rest->get("projects/$project_id/time/total", $params);
+    }
+
+    /**
      * Retrieve all To-do Item Times
      *
      * GET /todo_items/#{todo_item_id}/time_entries
@@ -105,7 +146,7 @@ class Time extends Model
      *
      * @param int $id
      * @param array $params
-     * @return TeamWorkPm\Response\Model
+     * @return \TeamWorkPm\Response\Model
      */
     public function getByTask($task_id, array $params = [])
     {
@@ -114,5 +155,45 @@ class Time extends Model
             throw new Exception('Invalid param task_id');
         }
         return $this->rest->get("todo_items/$task_id/$this->action", $params);
+    }
+
+    /**
+     * Retrieve all To-do Item Times
+     *
+     * GET /todo_items/#{todo_item_id}/time_entries
+     *
+     * Retrieves all of the time entries from a submitted todo item.
+     *
+     * @param int $id
+     * @param array $params
+     * @return \TeamWorkPm\Response\Model
+     */
+    public function getTotalByTask($task_id, array $params = [])
+    {
+        $task_id = (int) $task_id;
+        if ($task_id <= 0) {
+            throw new Exception('Invalid param task_id');
+        }
+        return $this->rest->get("tasks/$task_id/time/total", $params);
+    }
+
+    /**
+     * Retrieve all To-do Item Times
+     *
+     * GET /todo_items/#{todo_item_id}/time_entries
+     *
+     * Retrieves all of the time entries from a submitted todo item.
+     *
+     * @param int $id
+     * @param array $params
+     * @return \TeamWorkPm\Response\Model
+     */
+    public function getTotalByTaskList($task_list_id, array $params = [])
+    {
+        $task_id = (int) $task_list_id;
+        if ($task_id <= 0) {
+            throw new Exception('Invalid param task_list_id');
+        }
+        return $this->rest->get("tasklists/$task_list_id/time/total", $params);
     }
 }
